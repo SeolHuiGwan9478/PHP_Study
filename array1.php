9203dd083e75b158b47a209635b65865d665ef68
SeolHuiGwan9478 <shg5977@naver.com>
2021-07-06 15:09:13 +0900

    add function & array & hash

---
+++ array1.php
@@ -0,0 +1,10 @@
+<?php
+$member1 = ['egoing', 'k8805', 'sorialgi'];
+$member2 = array('egoing', 'k8805', 'sorialgi');
+echo $member1[0].'<br />';
+echo $member1[1].'<br />';
+echo $member1[2].'<br />';
+echo $member2[0].'<br />';
+echo $member2[1].'<br />';
+echo $member2[2].'<br />';
+?>
